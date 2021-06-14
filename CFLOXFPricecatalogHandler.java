5e71035f41cca615c1ab30a2ecb49bafb2a1c3a8
sabareesh007 <85455213+sabareesh007@users.noreply.github.com>
2021-06-14 17:24:13 +0530

    Delete CFLOXFPricecatalogHandler.java

--- CFLOXFPricecatalogHandler.java
+++
@@ -1,9 +0,0 @@
-package com.LogoPlatformTailor.customization;
-
-public class CFLOXFPricecatalogHandler {
-
-	public static  void main(String args[]) {
-		System.out.println("Hello World");
-	}
-
-}
